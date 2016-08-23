7bf3f55a5a575ab61d156e67d71b554033b13e83
Matt Kopacz <Matt@iMac-Mateusz.local>
2016-08-23 13:11:42 +0200

    ∏ symbol and sqrt function working. App without using MVC

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var userIsInTheMiddleOfTyppingANumber = false
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    @IBAction func pushedDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyppingANumber{
+            displayLabel.text = displayLabel.text! + digit
+        } else {
+            displayLabel.text = digit
+            userIsInTheMiddleOfTyppingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue:Double{
+        get {
+            return Double(displayLabel.text!)!
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+        
     }
-
-
+    
+    @IBAction func pushedOperator(sender: UIButton) {
+        if let mathematicalOperator = sender.currentTitle{
+            if mathematicalOperator == "∏" {
+                displayValue = M_PI
+            }
+            else if mathematicalOperator == "√"{
+                displayValue = sqrt(displayValue)
+            }
+        
+        }
+    
+    }
+    
 }
 
